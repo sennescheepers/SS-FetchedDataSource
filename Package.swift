8f4892d5b0faf95bebc831b27af5cb119b06fb57
Senne Scheepers <senne.scheepers@gmail.com>
2025-04-28 21:13:13 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,14 +1,26 @@
+// swift-tools-version:5.3
 import PackageDescription
+
 let package = Package(
     name: "SSFetchedDataSource",
-    platforms: [.iOS(.v16), .macOS(.v13)],
+    platforms: [
+        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+    ],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SSFetchedDataSource",
-            targets: ["SSFetchedDataSource"]),
+            targets: ["SSFetchedDataSource"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SSFetchedDataSource")
+            name: "SSFetchedDataSource",
+            exclude: ["instructions.md"]
+        )
     ]
 )
