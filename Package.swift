b7e48d8b6337849d906bd432b6fbf89fccadceec
sennescheepers <senne.scheeprs@gmail.com>
2025-04-29 20:22:11 +0200

    refactor sturcture

--- Package.swift
+++ Package.swift
@@ -12,9 +12,6 @@ let package = Package(
             name: "SSFetchedDataSource",
             targets: ["SSFetchedDataSource"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
